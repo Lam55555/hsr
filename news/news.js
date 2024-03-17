394eb7b23dbfa395525556525703ffd3398fcf43
Lam55555 <nguyenhoanglam121106@gmail.com>
2024-03-17 13:41:21 +0700

    y

---
+++ news/news.js
@@ -0,0 +1,181 @@
+news = {
+    phase1: [
+        {
+            link: "/homepage/homepage.html",
+            image: "/img/news/c5f2a0a0ca34f716b36b0603fd43af2c_7055457993374162577.jpg",
+            tittle: 'EP Honkai: Star Rail: "Theo Đuổi Xu Hướng (Phần 1)"',
+            content: "Ham muốn của con người phải được thỏa mãn thông qua sự phủ định, và tình yêu có nghĩa là trao đi thứ bạn không có cho người không cần nó.",
+            date: "16/2/2024"
+        },
+
+        {
+            link: "/homepage/homepage.html",
+            image: "/img/news/95d86206a4a20eec48f43c57e8d2d58d_2776416636598596786.jpg",
+            tittle: 'Honkai: Star Rail - Hoạt Hình Chủ Đề Cậu Bé Đồng Hồ: Chào mừng đến Thị Trấn Mộng Đẹp',
+            content: 'Cậu Bé Đồng Hồ thông minh dũng cảm bắt đầu hành trình phiêu lưu!  Tích tắc! Ông Chủ Đá đáng ghét thật xấu xa Tích tắc! Cứ luôn muốn đẩy Cậu Bé Đồng Hồ vào tình cảnh nguy hiểm; Những đừng lo lắng, tích tắ Cậu Bé Đồng Hồ thông minh, Cậu Bé Đồng Hồ dũng  Cậu Bé Đồng Hồ và những người bạn đáng tin cậy của  Sẽ bảo vệ giấc mơ, sẽ xua đuổi kẻ xấu!',
+            date: "13/2/2024"
+        },
+        
+        {
+            link: "/homepage/homepage.html",
+            image: "/img/news/88c6350f347401fdba3e591e0e86e85b_3647181104173706206.jpg",
+            tittle: 'Phim Ngắn Anime "Honkai: Star Rail": Tuổi Mới Bình An',
+            content: 'Cậu Bé Đồng Hồ thông minh dũng cảm bắt đầu hành trình phiêu lưu!  Tích tắc! Ông Chủ Đá đáng ghét thật xấu xa Tích tắc! Cứ luôn muốn đẩy Cậu Bé Đồng Hồ vào tình cảnh nguy hiểm; Những đừng lo lắng, tích tắ Cậu Bé Đồng Hồ thông minh, Cậu Bé Đồng Hồ dũng  Cậu Bé Đồng Hồ và những người bạn đáng tin cậy của  Sẽ bảo vệ giấc mơ, sẽ xua đuổi kẻ xấu!',
+            date: "8/2/2024"
+        },
+        
+        {
+            link: "/homepage/homepage.html",
+            image: "/img/news/68380f5440416bf561c37b9a118ea8b7_2417820660779588831.jpg",
+            tittle: 'Honkai: Star Rail Tiến Đến Trời Sao - [Misha: Trợ Thủ Của Bạn Tại Hành Tinh Lễ Hội]',
+            content: 'Chào mừng đến với Khách Sạn Mộng Mơ. Khi bạn lần đầu tiên đến với ██ này, có thể sẽ gặp phải một số rắc rối. Vì vậy, ██ đã đặc biệt sắp xếp người gác cửa Misha chu đáo, là ████ ở hành trình mộng mơ của bạn. Nếu bạn bạn cần bất kỳ sự giúp đỡ nào, hãy ██ với Misha, cậu ấy sẽ đảm bảo ████ của bạn.',
+            date: "7/2/2024"
+        },
+
+        {
+            link: "/homepage/homepage.html",
+            image: "/img/news/9b9a6addf172ad0ae14b935c458bb25d_8747902410689170814.jpg",
+            tittle: 'Lời Mời Lễ Hội',
+            content: 'Sự kiện web trở về "Lời Mời Lễ Hội" đã ra mắt, tham gia tối đa có thể nhận 540 Ngọc Ánh Sao và quà xịn khác!',
+            date: "6/2/2024"
+        },
+
+    ],
+    phase2: [
+            {
+                link: "/homepage/homepage.html",
+                image: "/img/news/c5f2a0a0ca34f716b36b0603fd43af2c_7055457993374162577.jpg",
+                tittle: 'EP Honkai: Star Rail: "Theo Đuổi Xu Hướng (Phần 1)"',
+                content: "Ham muốn của con người phải được thỏa mãn thông qua sự phủ định, và tình yêu có nghĩa là trao đi thứ bạn không có cho người không cần nó.",
+                date: "16/2/2024"
+            },
+    
+            {
+                link: "/homepage/homepage.html",
+                image: "/img/news/95d86206a4a20eec48f43c57e8d2d58d_2776416636598596786.jpg",
+                tittle: 'Honkai: Star Rail - Hoạt Hình Chủ Đề Cậu Bé Đồng Hồ: Chào mừng đến Thị Trấn Mộng Đẹp',
+                content: 'Cậu Bé Đồng Hồ thông minh dũng cảm bắt đầu hành trình phiêu lưu!  Tích tắc! Ông Chủ Đá đáng ghét thật xấu xa Tích tắc! Cứ luôn muốn đẩy Cậu Bé Đồng Hồ vào tình cảnh nguy hiểm; Những đừng lo lắng, tích tắ Cậu Bé Đồng Hồ thông minh, Cậu Bé Đồng Hồ dũng  Cậu Bé Đồng Hồ và những người bạn đáng tin cậy của  Sẽ bảo vệ giấc mơ, sẽ xua đuổi kẻ xấu!',
+                date: "13/2/2024"
+            },
+            
+            {
+                link: "/homepage/homepage.html",
+                image: "/img/news/88c6350f347401fdba3e591e0e86e85b_3647181104173706206.jpg",
+                tittle: 'Phim Ngắn Anime "Honkai: Star Rail": Tuổi Mới Bình An',
+                content: 'Cậu Bé Đồng Hồ thông minh dũng cảm bắt đầu hành trình phiêu lưu!  Tích tắc! Ông Chủ Đá đáng ghét thật xấu xa Tích tắc! Cứ luôn muốn đẩy Cậu Bé Đồng Hồ vào tình cảnh nguy hiểm; Những đừng lo lắng, tích tắ Cậu Bé Đồng Hồ thông minh, Cậu Bé Đồng Hồ dũng  Cậu Bé Đồng Hồ và những người bạn đáng tin cậy của  Sẽ bảo vệ giấc mơ, sẽ xua đuổi kẻ xấu!',
+                date: "8/2/2024"
+            },
+            
+            {
+                link: "/homepage/homepage.html",
+                image: "/img/news/68380f5440416bf561c37b9a118ea8b7_2417820660779588831.jpg",
+                tittle: 'Honkai: Star Rail Tiến Đến Trời Sao - [Misha: Trợ Thủ Của Bạn Tại Hành Tinh Lễ Hội]',
+                content: 'Chào mừng đến với Khách Sạn Mộng Mơ. Khi bạn lần đầu tiên đến với ██ này, có thể sẽ gặp phải một số rắc rối. Vì vậy, ██ đã đặc biệt sắp xếp người gác cửa Misha chu đáo, là ████ ở hành trình mộng mơ của bạn. Nếu bạn bạn cần bất kỳ sự giúp đỡ nào, hãy ██ với Misha, cậu ấy sẽ đảm bảo ████ của bạn.',
+                date: "7/2/2024"
+            },
+    
+            {
+                link: "/homepage/homepage.html",
+                image: "/img/news/9b9a6addf172ad0ae14b935c458bb25d_8747902410689170814.jpg",
+                tittle: 'Lời Mời Lễ Hội',
+                content: 'Sự kiện web trở về "Lời Mời Lễ Hội" đã ra mắt, tham gia tối đa có thể nhận 540 Ngọc Ánh Sao và quà xịn khác!',
+                date: "6/2/2024"
+            },
+    
+        
+    ]
+}
+
+
+for(let i of news.phase1){
+    
+
+
+    let image = document.createElement('img')
+    image.classList.add("img-news")
+    image.setAttribute('src',i.image)
+
+    let divanh = document.createElement('div')
+    divanh.classList.add('divanh')
+    divanh.appendChild(image) 
+
+    let divlon = document.createElement("div")
+    divlon.classList.add("divlon")
+    divlon.innerText = i.tittle
+
+
+    let divnho = document.createElement('div')
+    divnho.classList.add("divnho")
+    divnho.innerText = i.content
+
+    let date = document.createElement('div')
+    date.classList.add('date')
+    date.innerText = i.date
+
+    let divtext = document.createElement('div')
+    divtext.classList.add("divtext")
+    divtext.appendChild(divlon)
+    divtext.appendChild(divnho)
+    divtext.appendChild(date)
+
+    let container = document.createElement('div')
+    container.classList.add("container")
+    container.appendChild(divtext)
+
+    let  link = document.createElement('a')
+    link.setAttribute('href',i.link)
+    link.classList.add("ctn")
+    link.appendChild(divanh)
+    link.appendChild(container)
+    
+
+    document.getElementById("news-info").appendChild(link)
+}
+
+function add(){
+        for(let i of news.phase2){
+    
+
+
+            let image = document.createElement('img')
+            image.classList.add("img-news")
+            image.setAttribute('src',i.image)
+        
+            let divanh = document.createElement('div')
+            divanh.classList.add('divanh')
+            divanh.appendChild(image) 
+        
+            let divlon = document.createElement("div")
+            divlon.classList.add("divlon")
+            divlon.innerText = i.tittle
+        
+        
+            let divnho = document.createElement('div')
+            divnho.classList.add("divnho")
+            divnho.innerText = i.content
+        
+            let date = document.createElement('div')
+            date.classList.add('date')
+            date.innerText = i.date
+        
+            let divtext = document.createElement('div')
+            divtext.classList.add("divtext")
+            divtext.appendChild(divlon)
+            divtext.appendChild(divnho)
+            divtext.appendChild(date)
+        
+            let container = document.createElement('div')
+            container.classList.add("container")
+            container.appendChild(divtext)
+        
+            let  link = document.createElement('a')
+            link.setAttribute('href',i.link)
+            link.classList.add("ctn")
+            link.appendChild(divanh)
+            link.appendChild(container)
+            
+        
+            document.getElementById("news-info").appendChild(link)
+        }
+    
+}
